e0c5d5537602e791677e82f7fe0ee8db10279841
Brett Earle <brettearle@gmail.com>
2023-08-07 20:48:37 +0930

    spanish

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,19 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("say hello with first name", func(t *testing.T) {
-		got := Hello("Brett")
+		got := Hello("Brett", "")
 		want := "Hello, Brett"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say hello with empty string", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("say hello with spanish", func(t *testing.T) {
+		got := Hello("Brett", "Spanish")
+		want := "Hola, Brett"
+		assertCorrectMessage(t, got, want) 
+	})
 }
 
-func assertCorrectMessage(t *testing.T, got, want string) {
+func assertCorrectMessage(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
